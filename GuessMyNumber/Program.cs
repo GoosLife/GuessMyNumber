f3d12b891b26beccdcd7ef4050897965ba2d7c0a
Frederik Hvid Sørensen <fred09j3@zbc.dk>
2022-03-21 14:02:08 +0100

    Guess my number.

--- GuessMyNumber/Program.cs
+++ GuessMyNumber/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿Random rng = new Random();
+
+int number = rng.Next(1, 101);
+int guess;
+
+Console.WriteLine("Guess my number! (1-100).");
+
+do
+{
+    guess = Guess();
+} while (guess != number);
+
+static int Guess()
+{
+    string input;
+    int output;
+
+    input = Console.ReadLine();
+    if (!int.TryParse(input, out output))
+    {
+        Console.WriteLine("Please enter a number between 1 and 100.");
+    }
+
+    return output;
+}
+
+static void GiveHint(int guess, int number)
+{
+    if (guess > number)
+    {
+        Console.WriteLine("My number is smaller.");
+    }
+    else if (guess < number)
+    {
+        Console.WriteLine("My number is larger.");
+    }
+    else if (guess == number)
+    {
+        Console.WriteLine("Correct! My number was {0}", number);
+    }
+}
